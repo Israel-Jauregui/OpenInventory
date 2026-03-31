049e24ea164627a72d2117e20d312497a9d736ab
Joshua Hammond <joshuakhammond@gmail.com>
2026-03-30 22:36:46 -0400

    TEMP ROUTING

---
+++ frontend/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text} from 'react-native';
+import { Redirect } from 'expo-router';
+
+export default function EntryPoint(){
+
+    return(
+
+        <Redirect href="/login" />
+
+    );
+}
