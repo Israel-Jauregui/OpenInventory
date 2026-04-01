ddc88fbe20da23b65e131bf8232dde5c6628853c
SSpacesu <colinebutcher@gmail.com>
2026-04-01 14:17:50 -0400

    logout button

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,12 +1,15 @@
-import { Tabs } from 'expo-router';
+import { Tabs, useRouter} from 'expo-router';
+import { Button } from "react-native";
 
 function TabLayout(){
+    const router = useRouter();
 
     return(
 
         <Tabs>
             
-            <Tabs.Screen name="index" ></Tabs.Screen>.
+            <Tabs.Screen name="home" options={{headerRight: () => <Button title='Logout' onPress={() => router.replace('/login')} />}} />
+
         </Tabs>
     );
 
