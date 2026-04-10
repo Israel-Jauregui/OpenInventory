1a33c7af2767c2aedc053c72bf626cefc7ed2150
SSpacesu <colinebutcher@gmail.com>
2026-04-09 22:59:12 -0400

    tab delay

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
   return (
     <Stack> 
       <Stack.Screen name="(tabs)"   options={{ headerShown: false }} />
-      <Stack.Screen name="login"    options={{ headerShown: false, animation: 'slide_from_left' }} />
+      <Stack.Screen name="login"    options={{ headerShown: false, animation: 'slide_from_left', animationDuration: 275 }} />
     </ Stack>
 
   );
