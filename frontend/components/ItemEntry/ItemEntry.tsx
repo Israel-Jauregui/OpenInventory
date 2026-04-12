3074caf1bb97e320fe599eeb33dd6ca6b7b5b2a9
Joshua Hammond <joshuakhammond@gmail.com>
2026-04-11 22:11:22 -0400

    Updated item entry template

--- frontend/components/ItemEntry/ItemEntry.tsx
+++ frontend/components/ItemEntry/ItemEntry.tsx
@@ -2,14 +2,15 @@
 //ItemEntry.tsx: Component that represents a row for an individual item. Contains all relevant details / buttons for interacting with a given item.
 
 //MARK: Imports
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-//TODO: Add props that correspond to displayed data
+//TODO: Add props that correspond to displayed data. Background color could also be passed so that it alters between each subsequent row.
 export default function ItemEntry() {
 
-//MARK: Returned component
+    //MARK: Returned component
     return (<>
 
+        {/*BEGIN rowContainer*/}
         {//Main row container
         }<View style={styles.rowContainer}>
 
@@ -18,15 +19,39 @@ export default function ItemEntry() {
                 <Image style={styles.rowImage} />
             </View>
 
-            {//Container for item data labels such as name, UPC, etc.
+            {/*Container for item data labels such as name, UPC, etc.
+            TODO:  Add appropriate props to each Text component. 
+            Also consider conditionally rendering each label depending on whether they have an actual value.
+                
+            */
             }<View style={styles.labelsContainer}>
-                <Text style={{fontSize: 26}}>Item_Name</Text>
-                <Text>Quantity: {}</Text>
-                <Text>Category: {}</Text>
-                <Text>Brand: {}</Text>
-                <Text>Price: {}</Text>
-                <Text>UPC: {}</Text>
+                <Text style={{ fontSize: 26 }}>Item_Name</Text>
+                <Text>Quantity: { }</Text>
+                <Text>Category: { }</Text>
+                <Text>Brand: { }</Text>
+                <Text>Price: { }</Text>
+                <Text>UPC: { }</Text>
             </View>
+
+            {/*Container for buttons that modify or display item data TODO: Consider moving to component*/
+            }<View style={styles.buttonsContainer}>
+                {//Item description / view button
+                }<TouchableOpacity style={styles.button}>
+                    <Image style={{height: 25, width: 25, backgroundColor: "magenta"}}source={require("../../assets/images/itemDescIcon.png")}/>
+                </TouchableOpacity>
+
+                {//Edit item data button
+                }<TouchableOpacity style={styles.button}>
+                    <Image style={{height: 40, width: 40, backgroundColor: "blue"}}source={require("../../assets/images/editIcon.png")}/>
+                </TouchableOpacity>
+
+                {//Delete item master data
+                }<TouchableOpacity style={styles.button}>
+                    <Image style={{height: 25, width: 25, backgroundColor: "pink"}}source={require("../../assets/images/deleteIcon.png")}/>
+                </TouchableOpacity>
+            </View>
+
+            {/*END rowContainer*/}
         </View>
 
     </>);
@@ -76,8 +101,29 @@ const styles = StyleSheet.create(
 
             //10 pixels lower than rowContainer's fixed height
             height: 140,
+            width: 180,
 
             //FIXME: TEMPORARY COLORING FOR JUDGING DIMENSIONS
             backgroundColor: "grey"
         },
+        
+        buttonsContainer: {
+            //FIXME: Change back to row if needed
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 5,
+
+            marginLeft: 0,
+        },
+
+        button: {
+            justifyContent: "center",
+            alignItems: "center",
+
+            width: 40,
+            height: 40,
+
+            backgroundColor: "brown"
+        }
     });
