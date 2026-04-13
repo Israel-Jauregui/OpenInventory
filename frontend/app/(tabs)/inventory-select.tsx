63b60c27d6d2267b5be0d23350d9f2ae7dfe5859
Joshua Hammond <joshuakhammond@gmail.com>
2026-04-12 23:03:21 -0400

    Added modal for creating new inventory

--- frontend/app/(tabs)/inventory-select.tsx
+++ frontend/app/(tabs)/inventory-select.tsx
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, Dimensions } from "react-native";
+import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, Dimensions, Modal, TextInput } from "react-native";
+import { useState } from 'react';
 import { useRouter } from "expo-router";
 
 const { width } = Dimensions.get("window");
@@ -11,6 +12,7 @@ const PLACEHOLDER_INVENTORIES = [
 ];
 
 export default function InventorySelect() {
+  const [modalVisible, setModalVisible] = useState(false);
   const router = useRouter();
 
   const handleSelect = (inventory: { id: string; name: string }) => {
@@ -23,9 +25,50 @@ export default function InventorySelect() {
 
   return (
     <View style={styles.container}>
+      {/*Modal for creating a new inventory*/
+      }<Modal
+        transparent={true}
+        animationType={"slide"}
+        visible={modalVisible}>
+        {/*Centered container for the modal*/
+        }<View style={{ alignItems: "center" }}>
+          <View style={styles.createInventoryModal}>
+            <TouchableOpacity 
+            style={
+              {
+                position: "absolute",
+                top: 20,
+                right: 20,
+              }}
+            onPress={()=>{setModalVisible(false)}}>
+              <Image
+                style={
+                  {
+                    height: 40,
+                    width: 40,
+                    transform: [{ rotate: "45deg" }]
+                  }}
+
+                source={require("../../assets/images/plusIcon.png")}
+              />
+            </TouchableOpacity>
+
+            <Text style={styles.createInventoryHeader}>Create a new inventory</Text>
+            <TextInput style={styles.createInventoryNameField} placeholder="Inventory Name"></TextInput>
+            <Text style={styles.createInventoryAltText}>All you need right now is the inventory's name; you may add items or configurations later at any time.</Text>
+            <TouchableOpacity
+              style={styles.createInventoryButton}>
+              <Text style={styles.createInventoryButtonText}>Create</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </Modal >
+
       <Text style={styles.heading}>Select an Inventory</Text>
 
-      <FlatList
+      {//Could possibly be changed to a SectionList to show each category with a large label, though isn't strictly necessary
+      } <FlatList
         data={PLACEHOLDER_INVENTORIES}
         keyExtractor={(item) => item.id}
         contentContainerStyle={styles.list}
@@ -35,20 +78,41 @@ export default function InventorySelect() {
             onPress={() => handleSelect(item)}
           >
             <Text style={styles.cardText}>{item.name}</Text>
-            <Image style={styles.cardChevron} source={require("../../assets/images/chevronRight.png")} />
+            <Image style={styles.cardIcon} source={require("../../assets/images/chevronRight.png")} />
           </TouchableOpacity>
         )}
-      />
-    </View>
+        //Button for creating a new inventory instance
+        ListFooterComponent={
+          <TouchableOpacity
+            style={
+              [styles.card,
+              {
+                backgroundColor: "rgba(11, 74, 73, 0.71)",
+                borderWidth: 4,
+                borderColor: "#6fbeff"
+
+              }]}
+            onPress={() => { setModalVisible(true) }}
+          >
+            <Text style={[styles.cardText, { color: "white", fontWeight: 600 }]}>Create new inventory</Text>
+            <Image style={styles.cardIcon} source={require("../../assets/images/plusIcon.png")} />
+          </TouchableOpacity>} />
+
+
+
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5",
     alignItems: "center",
+
+    backgroundColor: "#f5f5f5",
+
     paddingTop: 20,
+
   },
   heading: {
     fontSize: 26,
@@ -59,6 +123,7 @@ const styles = StyleSheet.create({
   list: {
     paddingHorizontal: 20,
     paddingBottom: 40,
+
   },
   card: {
 
@@ -86,11 +151,85 @@ const styles = StyleSheet.create({
     color: "#39a2f8",
   },
 
-  cardChevron: {
+  cardIcon: {
     position: "absolute",
     right: 20,
 
     height: 35,
     width: 35
   },
+
+  //MARK: Create inventory modal
+  //BEGIN Create inventory modal
+  createInventoryModal: {
+    alignItems: "center",
+    justifyContent: "center",
+
+    //Allows for empty space above modal
+    top: "7%",
+
+    height: "95%",
+    width: "98%",
+
+    backgroundColor: "white",
+
+    borderRadius: 20,
+  },
+
+  createInventoryHeader: {
+    marginBottom: "20%",
+
+
+    fontSize: 28,
+    fontWeight: "600",
+
+  },
+
+  createInventoryNameField: {
+    padding: 20,
+
+    width: "80%",
+
+    fontSize: 22,
+
+    borderColor: "#6fbeff",
+    borderWidth: 1,
+    borderRadius: 20,
+
+    backgroundColor: "white",
+  },
+
+  createInventoryAltText: {
+    padding: 10,
+
+    width: "82%",
+
+    color: "grey"
+
+
+  },
+
+  createInventoryButton: {
+    marginTop: "15%",
+    padding: 20,
+
+    alignItems: "center",
+
+    width: "80%",
+
+    backgroundColor: "rgba(11, 74, 73, 0.71)",
+
+    borderWidth: 4,
+    borderRadius: 12,
+    borderColor: "#6fbeff",
+
+  },
+
+  createInventoryButtonText: {
+
+    color: "white",
+    fontSize: 28,
+    fontWeight: "600",
+  },
+  //END Create inventory modal
 });
