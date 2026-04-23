d1e276da1b9e1d4af13d958bde09a7dfb6d938cd
Joshua Hammond <joshuakhammond@gmail.com>
2026-04-23 13:24:19 -0400

    Added comments in items.tsx detailing potential ways to implement state management for items in general, though context + useState or useReducer could be used instead until either an issue arises or it becomes too complex / cumbersome to manage.

--- frontend/app/(tabs)/items.tsx
+++ frontend/app/(tabs)/items.tsx
@@ -1,7 +1,7 @@
 
 import { ScrollView } from 'react-native';
 
-import { useEffect, useContext } from 'react'; 
+import { useEffect, useContext } from 'react';
 
 import { useCurrentInventoryContext } from '@/contexts/InventoryNamesContext/CurrentInventoryContext';
 
@@ -12,20 +12,27 @@ import ItemsSearchBar from '@/components/ItemsSearchBar/ItemsSearchBar';
 //END Custom component imports
 
 
-//TODO: Incorporate fetch that displays all items
+/*
+    TODO: Incorporate fetch that displays all items. 
+    Managing state of each item could be done with Context API + useReducer(), though
+    more research should be done towards the libraries Zustand and TanStack (also known as React Queery) since context may cause
+    unnecessary rerenders when such context is updated.
+    This explains the general difference between Context and state management libraries
+    https://blog.isquaredsoftware.com/2021/01/context-redux-differences/
+*/
 export default function ItemsView() {
 
     const { currentInventory } = useCurrentInventoryContext();
 
-    
-    //TODO: Convert ScrollView into a SectionList that displays items under each category. 
+
+    //TODO: Convert ScrollView into either a FlatList or SectionList that displays items (under each category if SectionList). 
     return (<>
         <InventoryHeader inventoryName={currentInventory.invName} />
         <ItemsSearchBar />
 
 
         {//TEMPORARY
-        }<ScrollView contentContainerStyle={{alignItems: "center", justifyContent: "center", gap: 8}}>
+        }<ScrollView contentContainerStyle={{ alignItems: "center", justifyContent: "center", gap: 8 }}>
             <ItemEntry />
             <ItemEntry />
             <ItemEntry />
