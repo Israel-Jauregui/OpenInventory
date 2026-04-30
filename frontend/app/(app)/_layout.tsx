153e7b88547088d039bd0d2c4a22825292035ca5
SSpacesu <colinebutcher@gmail.com>
2026-04-30 15:02:46 -0400

    fixed back button for manage user modal

--- frontend/app/(app)/_layout.tsx
+++ frontend/app/(app)/_layout.tsx
@@ -20,7 +20,7 @@ export default function AppRoutesLayout() {
                 <Stack.Screen name="(tabs)" />
                 {//Remove below Stacks if necessary aside from scanner
                 }
-                <Stack.Screen name="inventory/manage-users" options={{headerShown: true, headerTitle: "Manage Users"}} />
+                <Stack.Screen name="inventory/manage-users" options={{headerShown: true, headerTitle: "Manage Users", headerBackTitle: "Home"}} />
                 <Stack.Screen name="inventory/invite-users" options={{headerShown: true, headerTitle: "Invite Users"}} />
                 <Stack.Screen name="inventory/item/create" options={{presentation: "modal", headerShown: true, headerTitle: "Create Item"}}/>
                 <Stack.Screen name="inventory/item/[itemId]/index" options={{ headerShown: true, headerTitle: "Item Details" }} />
